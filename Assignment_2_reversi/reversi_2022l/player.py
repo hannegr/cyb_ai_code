5dbe5fc69b4032b8b114a4c3d6d735f04c402e7d
hannegr <14hhannegrete@gmail.com>
2022-03-08 12:29:34 +0100

    started working on it

--- Assignment_2_reversi/reversi_2022l/player.py
+++ Assignment_2_reversi/reversi_2022l/player.py
@@ -1,16 +1,23 @@
+import numpy as np
+from game_board import GameBoard
+
 class MyPlayer():
-    '''Template Docstring for MyPlayer, look at the TODOs''' # TODO a short description of your player
+    '''My player is the best player and will win any tournament! Just wait!!!''' # TODO a short description of your player
 
     def __init__(self, my_color,opponent_color, board_size=8):
-        self.name = 'username' #TODO: fill in your username
+        self.name = 'alvhehan'
         self.my_color = my_color
         self.opponent_color = opponent_color
         self.board_size = board_size
+        
 
     def move(self,board):
         # TODO: write you method
         # you can implement auxiliary fucntions, of course
-        return (0, 0)
+        if(self.get_all_valid_moves == None): 
+            return None 
+        
+        return (2, 1)
 
     def __is_correct_move(self, move, board):
         dx = [-1, -1, -1, 0, 1, 1, 1, 0]
@@ -50,4 +57,31 @@ class MyPlayer():
             print('No possible move!')
             return None
         return valid_moves
+    
+    class SearchTree(): 
+        def __init__(self): 
+            self.player_utilities = np.array([50, 10, 5, 2, -1, -25, -50])
+            self.opponent_utilities = -self.player_utilities
+            self.search_depth = 8 #for alpha/beta algorithm 
+            self.alpha = -1000
+            self.beta = 1000
+        
+
+            
+    class Node(): 
+        def __init__(self, parent, depth):
+            self.parent = parent 
+            self.depth = depth 
+            self.cost = None 
+            
+if __name__ == "__main__": 
+    board = GameBoard()
+    board.print_board()
+    
+    agent = MyPlayer(0,1)
+    #print(agent.move(board))
+    print(agent.get_all_valid_moves(board))
+    #moves = agent.get_all_valid_moves(board)
+        
+            
     
