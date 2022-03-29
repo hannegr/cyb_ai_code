446fa4d24ac7e1045e620419c4ccfe459e4efe94
hannegr <14hhannegrete@gmail.com>
2022-03-29 18:39:04 +0200

    works, but not optimal

--- Assignment_3_sdp/kuimaze/mdp_agent.py
+++ Assignment_3_sdp/kuimaze/mdp_agent.py
@@ -1,6 +1,7 @@
 from cmath import inf
 import collections
 import random
+import numpy as np 
 
 import kuimaze.maze
 
@@ -11,22 +12,23 @@ def find_policy_via_value_iteration(problem, discount_factor, epsilon):
     '''
     value = 0.0
     all_states = problem.get_all_states()
-    value_list = []
+    value_list = [] 
+    
     for state_index in range(len(all_states)): 
-        value_list.append([all_states[state_index], None, value, value])#[0], all_states[state_index][1]])
+        value_list.append([all_states[state_index], None, value, value])        
     while True:
         delta = 0.0
         for state_value_index in range(len(value_list)): 
             reward = value_list[state_value_index][0][2]
             updated_value_list = sum_probability_values(problem, state_value_index, value_list)
             new_state_value = reward + discount_factor*updated_value_list
-            value_difference = abs(value_list[state_value_index][3] - new_state_value)
+            value_difference = value_list[state_value_index][3] - new_state_value #abs(value_list[state_value_index][3] - new_state_value)
             if(value_difference > delta): 
                 delta = value_difference
             value_list[state_value_index][3] = new_state_value
         if delta <= (epsilon*(1-discount_factor)*discount_factor): 
             break   
-    return turn_value_list_into_dictionary(value_list)
+    return turn_list_into_dictionary(value_list)
 
 def sum_probability_values(problem, state_value_index, value_list):
     '''
@@ -41,7 +43,6 @@ def sum_probability_values(problem, state_value_index, value_list):
         for next_state in next_states_and_probs: 
             for value in value_list: 
                 if value[0][0:2] in next_state:
-                #if next_state == value[0][0:2]: 
                     sum_action_values += value[3]*next_state[1]
         
         if sum_action_values > max_value: 
@@ -51,13 +52,15 @@ def sum_probability_values(problem, state_value_index, value_list):
             
     return max_value
 
+    
 
-def turn_value_list_into_dictionary(value_list): 
+
+def turn_list_into_dictionary(mdp_list): 
     '''
     TODO write a description here. 
     '''
     state_action_dictionary = {}
-    for state_values in value_list:
+    for state_values in mdp_list:
         #state_values[1] contains the state which we want to be the key, and state_values[2] 
         #contains the optimal action. 
         state_action_dictionary[state_values[0][0:2]] = state_values[1]
@@ -70,4 +73,56 @@ def find_policy_via_policy_iteration(problem, discount_factor):
     '''
     TODO write a description here. 
     '''
-    return None 
+    V = 0.0
+    all_states = problem.get_all_states()
+    policy_list = []
+    changed = True
+    for state in all_states: 
+        policy_list.append([state, random.choice(list(problem.get_actions(state))), V]) #trenger muligens ikke V. 
+    while changed: 
+        changed = False
+        for policy_index in range(len(policy_list)): 
+            policy_evaluation(problem, policy_index, policy_list, discount_factor)
+        for policy_index in range(len(policy_list)):
+            better_action = better_max_action(problem, policy_index, policy_list)
+            if (better_action != None) and better_action != policy_list[policy_index][1]: #muligens heller sammenligne verdier her!!!
+                #better_max_action: return first the value of the maximum action, and then the best action as the second element! 
+                policy_list[policy_index][1] = better_action
+                changed = True 
+    return turn_list_into_dictionary(policy_list)
+
+
+def policy_evaluation(problem, policy_list_index, policy_list, discount_factor): 
+    '''
+    TODO write a description here. 
+    '''
+    policy_value = 0
+    next_states_and_probs = problem.get_next_states_and_probs(policy_list[policy_list_index][0], policy_list[policy_list_index][1])
+    for next_state in next_states_and_probs: 
+            for policy in policy_list: 
+                if policy[0][0:2] in next_state:
+                    policy_value += policy[2]*next_state[1]
+    policy_value = policy_list[policy_list_index][0][2] + discount_factor*policy_value
+    policy_list[policy_list_index][2] = policy_value
+    #return policy_value #do I even need to return anything here? 
+
+
+
+def better_max_action(problem, policy_index, policy_list):
+    '''
+    TODO write a description here. 
+    ''' 
+    max_value = -inf
+    best_action = None
+    all_actions = problem.get_actions(policy_list[policy_index][0])
+    for action in all_actions: 
+        sum_action_values = 0
+        next_states_and_probs = problem.get_next_states_and_probs(policy_list[policy_index][0], action)
+        for next_state in next_states_and_probs: 
+            for policy in policy_list: 
+                if policy[0][0:2] in next_state:
+                    sum_action_values += policy[2]*next_state[1]
+        if sum_action_values > max_value: 
+            max_value = sum_action_values
+            best_action = action           
+    return best_action
